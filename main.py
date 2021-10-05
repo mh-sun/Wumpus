ecaab4e3f2a262f3acf00da9d7d2bc8b9ed5b519
mh-sun <bsse1025@iit.du.ac.bd>
2021-10-05 12:57:49 +0600

    Initial

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 pygame.init()
 
 screen = pygame.display
+screen_mode = screen.set_mode((800, 600))
 
-screen.set_mode((800, 600))
 screen.set_caption("Wumpus World")
 screen.set_icon(pygame.image.load('./res/wumpus.png'))
 
@@ -13,3 +13,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen_mode.fill((255, 255, 255))
+    screen.update()
